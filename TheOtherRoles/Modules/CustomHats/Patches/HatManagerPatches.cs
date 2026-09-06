f40a2430c04afdc67dc0fce71d70f6d0b87e0418
DatGamet <gamet@streame.gg>
2026-09-06 16:09:50 +0200

    Fix: Crash if race on Hats

--- TheOtherRoles/Modules/CustomHats/Patches/HatManagerPatches.cs
+++ TheOtherRoles/Modules/CustomHats/Patches/HatManagerPatches.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Cpp2IL.Core.Extensions;
 using HarmonyLib;
+using UnityEngine;
 
 namespace TheOtherRoles.Modules.CustomHats.Patches;
 
@@ -10,22 +11,35 @@ internal static class HatManagerPatches
 {
     private static bool isRunning;
     private static bool isLoaded;
-    private static List<HatData> allHats;
+    private static float nextMergeAttempt;
+    private const float MergeCooldownSeconds = 1f;
 
     [HarmonyPatch(nameof(HatManager.GetHatById))]
     [HarmonyPrefix]
     private static void GetHatByIdPrefix(HatManager __instance)
     {
         if (isRunning || isLoaded) return;
+        if (CustomHatManager.UnregisteredHats.Count == 0)
+        {
+            isLoaded = true;
+            return;
+        }
+
+        if (Time.realtimeSinceStartup < nextMergeAttempt) return;
+        nextMergeAttempt = Time.realtimeSinceStartup + MergeCooldownSeconds;
+
         isRunning = true;
         // Maybe we can use lock keyword to ensure simultaneous list manipulations ?
-        allHats = __instance.allHats.ToList();
+        // -> I think lock only lbocks other threads from changing the array, but this seems to happen in one thread
+        var allHats = __instance.allHats.ToList();
         var cache = CustomHatManager.UnregisteredHats.Clone();
+        var added = false;
         foreach (var hat in cache)
             try
             {
                 allHats.Add(CustomHatManager.CreateHatBehaviour(hat));
                 CustomHatManager.UnregisteredHats.Remove(hat);
+                added = true;
             }
             catch
             {
@@ -36,7 +50,8 @@ internal static class HatManagerPatches
             isLoaded = true;
         cache.Clear();
 
-        __instance.allHats = allHats.ToArray();
+        // only touch hats if something actually changed
+        if (added) __instance.allHats = allHats.ToArray();
     }
 
     [HarmonyPatch(nameof(HatManager.GetHatById))]
